a7f8c615eec8fe225279eee38584d0ff233727ec
Umer <umersufyan@gmail.com>
2022-02-17 15:27:48 +0500

    Initial Commit

---
+++ EssentialApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EssentialApp
+//
+//  Created by Apple on 17/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
