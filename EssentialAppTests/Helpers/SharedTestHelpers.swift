f20e6ad6c1a1afa02d76eb12852c285f60b4e5f0
Umer <umersufyan@gmail.com>
2022-02-18 16:32:30 +0500

    Extract test helpers into a shared scope to remove duplication

---
+++ EssentialAppTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,20 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialAppTests
+//
+//  Created by Apple on 18/02/2022.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
+
+func anyData() -> Data {
+    return Data("any data".utf8)
+}
