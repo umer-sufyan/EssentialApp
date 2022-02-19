5f1c69a13e9126666a815c01c3dd4af54e56a0bc
Umer <umersufyan@gmail.com>
2022-02-19 17:29:03 +0500

    Displays remote feed on launch when customer has connectivity

--- EssentialAppUITests/EssentialAppUIAcceptanceTests.swift
+++ EssentialAppUITests/EssentialAppUIAcceptanceTests.swift
@@ -14,11 +14,16 @@ class EssentialAppUIAcceptanceTests: XCTestCase {
         
         app.launch()
         
-        let feedCells = app.cells.matching(identifier: "feed-image-cell")
-        XCTAssertEqual(feedCells.count, 22)
+        _ = app.waitForExistence(timeout: 20)
         
-        let firstImage = app.images.matching(identifier: "feed-image-view").firstMatch
-        XCTAssertTrue(firstImage.exists)
+        XCTAssertEqual(app.cells.count, 22)
+            
+        
+        XCTAssertTrue(app.cells.firstMatch.exists)
+            
     }
+    
+    
+    
     
 }
