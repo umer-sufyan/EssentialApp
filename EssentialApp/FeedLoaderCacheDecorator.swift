f0af7ee68fcfafdbb140938a003fed754c78d37b
Umer <umersufyan@gmail.com>
2022-02-19 11:36:45 +0500

    Move FeedLoaderCacheDecorator to production

---
+++ EssentialApp/FeedLoaderCacheDecorator.swift
@@ -0,0 +1,26 @@
+//
+//  FeedLoaderCacheDecorator.swift
+//  EssentialApp
+//
+//  Created by Apple on 19/02/2022.
+//
+import EssentialFeedFramework
+
+public final class FeedLoaderCacheDecorator: FeedLoader {
+    private let decoratee: FeedLoader
+    private let cache: FeedCache
+    
+    public init(decoratee: FeedLoader, cache: FeedCache) {
+        self.decoratee = decoratee
+        self.cache = cache
+    }
+    
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
+        decoratee.load { [weak self] result in
+            completion(result.map { feed in
+                self?.cache.save(feed) { _ in }
+                return feed
+            })
+        }
+    }
+}
