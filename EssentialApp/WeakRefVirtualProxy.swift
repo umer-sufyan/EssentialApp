436ded03dc2d2a6d9d5ea4e64e88023e14aa5ad2
Umer <umersufyan@gmail.com>
2022-03-03 15:30:50 +0500

    Move Feed UI composition details from the EssentialFeediOS to the EssentialApp module (Composition Root)

---
+++ EssentialApp/WeakRefVirtualProxy.swift
@@ -0,0 +1,36 @@
+//
+//  WeakRefVirtualProxy.swift
+//  EssentialFeedFramworkiOS
+//
+//  Created by Apple on 17/01/2022.
+//
+import UIKit
+import EssentialFeedFramework
+import EssentialFeedFramworkiOS
+
+final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedErrorView where T: FeedErrorView {
+    func display(_ viewModel: FeedErrorViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel) {
+        object?.display(viewModel)
+    }
+}
+
+extension WeakRefVirtualProxy: FeedImageView where T: FeedImageView, T.Image == UIImage {
+    func display(_ model: FeedImageViewModel<UIImage>) {
+        object?.display(model)
+    }
+}
+
