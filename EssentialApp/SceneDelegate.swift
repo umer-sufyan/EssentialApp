e9a24bbcd36348fff870c85a2c70941c3a51620d
umer-sufyan <umer-sufyan@users.noreply.github.com>
2022-03-29 09:24:32 +0500

    Remove unnecessary Main storyboard - the initial set up happens in code

--- EssentialApp/SceneDelegate.swift
+++ EssentialApp/SceneDelegate.swift
@@ -42,6 +42,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
         
+        window = UIWindow(windowScene: scene as! UIWindowScene)
+        
+        
         configureWindow()
         
         
@@ -68,6 +71,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                     fallback: FeedImageDataLoaderCacheDecorator(
                         decoratee: remoteImageLoader,
                         cache: localImageLoader))))
+        
+        window?.makeKeyAndVisible()
         }
     
     func sceneDidDisconnect(_ scene: UIScene) {
